9a0df1f45f231382c8b42f45c432cfc24e10655d
brunasfenandes <bruna.fenandes@outlook.com>
2025-03-06 14:16:44 -0500

    changed newsletter content

--- src/components/Newsletter/NewsLetter.jsx
+++ src/components/Newsletter/NewsLetter.jsx
@@ -1,17 +1,16 @@
-import { useState, useEffect } from "react";
 import { Col, Row } from "react-bootstrap";
 import './Newsletter.scss';
 
-export default function Newsletter({ status, message, onValidated }) {
+export default function Newsletter() {
 
   return (
       <Col lg={12}>
         <div className="newsletter-bx wow slideInUp">
           <Row >
             <Col lg={12} md={6} xl={5}>
-              <h3>Your Motivational Phrase or About Me</h3>
-              <p>"Success is the sum of small efforts, repeated day in and day out."</p>
-              <p>Or tell a little bit more about yourself, your story, or your goals!</p>
+              <h3>Bits, Bugs, and Brain Teasers</h3>
+              <p>Did you know? The first computer bug was a literal bug! In 1947, engineers found a moth trapped in a relay, causing the malfunction.</p>
+              <p>That's why programmers prefer the dark mode. The light attracts bugs!</p>
             </Col>
           </Row>
         </div>
